5ecbc212830bf1a1fb14a572fc0a7f92cc02b111
mjmj0217 <mjmj0217@naver.com>
2022-03-28 20:06:23 +0900

    change folders

---
+++ week3/app/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+//라우팅
+const home = require("./src/routes/index");
+
+//앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use(express.static(`${__dirname}/src/public`))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/", home);
+
+
+module.exports = app;
