1be5ce88c2375ae0832d090ddf5e6847edb9294c
KYUUUW <duffufk1202@naver.com>
2022-04-03 05:31:06 +0900

    Connect db

--- week5/src/model/User.ts
+++ week5/src/model/User.ts
@@ -1,9 +1,48 @@
 import { faker } from "@faker-js/faker";
-import { v4 as uuidv4 } from "uuid";
+import { Prisma } from "@prisma/client";
+import { db } from "../db";
 
-export class UserModel {
-  id = uuidv4();
-  name = faker.name.lastName();
-  alias = faker.name.firstName();
-  avatarUrl = faker.image.avatar();
+type UserProps = Prisma.UserCreateInput;
+export class User {
+  props: UserProps;
+
+  constructor(props: UserProps) {
+    this.props = props;
+  }
+
+  static async findById(id: string) {
+    const r = await db.user.findUnique({
+      where: {
+        UserID: id,
+      },
+    });
+
+    if (!r) {
+      throw new Error("Not found");
+    }
+    return new User(r);
+  }
+
+  static async findByNameAndPw(name: string, pw: string) {
+    const r = await db.user.findFirst({
+      where: {
+        Name: name,
+        Password: pw,
+      },
+    });
+
+    if (!r) {
+      throw new Error("Not found");
+    }
+    return new User(r);
+  }
+
+  persist() {
+    db.user.update({
+      where: {
+        UserID: this.props.UserID,
+      },
+      data: this.props,
+    });
+  }
 }
